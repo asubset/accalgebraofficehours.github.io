504480b10142422c6f5eb042cd3f9f557596f382
accalgebraofficehours <147888375+accalgebraofficehours@users.noreply.github.com>
2025-07-25 20:27:20 -0600

    Update example.js

--- example.js
+++ example.js
@@ -1,8 +1,1 @@
-fetch('https://example.com/data/sessions.json')
-  .then(res => res.json())
-  .then(data => {
-    const PREDEFINED_SESSIONS = data;
-    console.log(PREDEFINED_SESSIONS);
-    // Do something with the sessions
-  })
-  .catch(err => console.error('Failed to fetch sessions:', err));
+
